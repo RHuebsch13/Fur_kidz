9e502de0a2ead6706c18e454ec479a1f7c7898b9
Roxann Huebsch <roxannhuebsch@outlook.com>
2024-10-19 09:44:49 +0000

    add products app, models and fixtures

---
+++ products/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+from django.db import models
+
+
+class ProductsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'products'
+
+
