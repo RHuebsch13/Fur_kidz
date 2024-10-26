d3dc0f84aae6906979249b141e4214d1d96888d5
Roxann Huebsch <roxannhuebsch@outlook.com>
2024-10-26 14:46:11 +0000

    add sorting

--- products/urls.py
+++ products/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
+    path('<product_id>', views.product_detail, name='product_detail'),
     path('category/<int:category_id>/', views.category_products, name='category_products'),
-    path('<int:product_id>/', views.product_detail, name='product_detail'),  # Ensure this line is present
 ]
