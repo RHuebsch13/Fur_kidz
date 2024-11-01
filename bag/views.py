036249faf275634b1a152313a51dba4418d3411a
Roxann Huebsch <roxannhuebsch@outlook.com>
2024-11-01 11:22:24 +0000

    add css for toast messages and bag preview

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-
+from django.shortcuts import get_object_or_404
 from products.models import Product
 
 # Create your views here.
@@ -13,13 +13,14 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
     
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quanity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.name} to your bag')
@@ -28,13 +29,25 @@ def add_to_bag(request, item_id):
     return redirect(redirect_url)
 
 def remove_from_bag(request, item_id):
-    bag = request.session.get('bag', {})
-    if item_id in bag:
+    """ Remove a quantity of the specified product from the shopping bag """
+    try:
+        product = get_object_or_404(Product, pk=item_id)
+        bag = request.session.get('bag', {})
         
-        if bag[item_id] > 1:
-            bag[item_id] -= 1
-        else:
-            del bag[item_id]  
-        request.session['bag'] = bag
-    return redirect('view_bag')
+        if item_id in bag:
+            if bag[item_id] > 1:
+                bag[item_id] -= 1
+                messages.success(request, f'Decreased quantity of {product.name} in your bag.')
+            else:
+                del bag[item_id] 
+                messages.success(request, f'Removed {product.name} from your bag.') 
+                
+            request.session['bag'] = bag
+        
+        return redirect('view_bag')
+        
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
+
     
