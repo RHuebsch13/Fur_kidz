bef496b619a843b03e15445f246eff9dfa7c87fc
Roxann Huebsch <roxannhuebsch@outlook.com>
2024-11-05 22:55:15 +0000

    email verfication debug add mandatory verification

--- Fur_kidz/settings.py
+++ Fur_kidz/settings.py
@@ -224,5 +224,8 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
+    ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 
 
